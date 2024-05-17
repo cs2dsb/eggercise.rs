65b1d8fe84d506f32ee234fda5a4c90e05d10ddb
daniel_ <cs2dsb@gmail.com>
2024-05-17 15:38:02 +0100

    Attempting to reduce unnecessary rebuilds

--- crates/server/build.rs
+++ crates/server/build.rs
@@ -177,19 +177,34 @@ fn main() -> Result<(), anyhow::Error> {
         "cargo:rerun-if-changed={}",
         path_to_str(&worker_info.manifest_dir)
     );
-    println!(
-        "cargo:rerun-if-changed={}",
-        path_to_str(&server_info.manifest_dir.join("assets"))
-    );
-
+    
     let is_release_build = !cfg!(debug_assertions);
-
     let server_dir = server_info.manifest_dir.clone();
     let out_dir = PathBuf::from(env::var("OUT_DIR")?);
     let wasm_dir = out_dir.join("wasm");
     let wasm_dir_str = path_to_str(&wasm_dir);
     let assets_dir = server_dir.join("assets");
     let server_wasm_dir = assets_dir.join("wasm");
+
+    // Add everything in the assets folder *except* the wasm dir to rerun-if-changed
+    // This won't work for new files in the root but this is an acceptable tradeoff
+    // to prevent rebuilding every time the wasm folder is touched. The alternative would
+    // be to diff the output of this build with the wasm folder and not update it if it 
+    // hasn't changed but this still requires running build.rs every time 
+    for f in glob(&format!(
+        "{}/**/*",
+        assets_dir.to_str().expect("Invalid assets_dir path")
+    ))?
+        .into_iter()
+        .collect::<Result<Vec<_>, _>>()?
+        .into_iter()
+        .filter(|f| ! f.starts_with(&server_wasm_dir))
+    {
+        println!(
+            "cargo:rerun-if-changed={}",
+            path_to_str(&f)
+        );
+    }
 
     p!("Out path: {:?}", out_dir);
     let CrateInfo {
