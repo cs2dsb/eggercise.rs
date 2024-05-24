fa2a84d9495af39e5e51c0a406d0ed2b8c1ca2c1
daniel_ <cs2dsb@gmail.com>
2024-05-24 17:49:46 +0100

    Added null & undefined checks around JSValue -> rust type conversions

---
+++ crates/client/src/utils/mod.rs
@@ -0,0 +1,32 @@
+use std::{any::type_name, error::Error};
+
+use leptos::logging::error;
+use shared::api::error::FrontendError;
+use wasm_bindgen::JsValue;
+
+pub trait JsValueIntoOk<R, E>: Sized 
+where  
+    Self: Into<R>,
+    E: Error,
+{
+    fn ok(self) -> Result<R, FrontendError<E>>;
+}
+
+impl<J, R, E> JsValueIntoOk<R, E> for J 
+where 
+    J:  Into<R> +
+        AsRef<JsValue>,
+    E: Error,
+{
+    fn ok(self) -> Result<R, FrontendError<E>> {
+        let jsvalue: &JsValue = self.as_ref();
+        if jsvalue.is_null() || jsvalue.is_undefined() {
+            let inner = format!("Failed to convert {} to {} because it was null or undefined", type_name::<Self>(), type_name::<R>());
+            error!("{inner}");
+            Err(FrontendError::Js { inner })
+        } else {
+            Ok(self.into())
+        }
+    }
+
+}
