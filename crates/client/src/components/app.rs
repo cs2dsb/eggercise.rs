241feb7bb1b285242028f80c651f249d04ea4828
daniel_ <cs2dsb@gmail.com>
2024-05-28 21:21:14 +0100

    SqlitePromiser provide_context. Moved test sql calls into a resource in App

--- crates/client/src/components/app.rs
+++ crates/client/src/components/app.rs
@@ -1,27 +1,50 @@
-use leptos::{component, create_signal, view, IntoView, SignalGet};
+use leptos::{component, create_local_resource, create_signal, view, IntoView, SignalGet, Transition, ErrorBoundary, SignalWith, CollectView};
 use leptos_router::Router;
 
-use crate::{AppNav, AppRoutes, components::OnlineCheck};
+use crate::{components::OnlineCheck, db::migrations::{self, MigrationError}, utils::sqlite3::SqlitePromiser, AppNav, AppRoutes};
 
 #[component]
 pub fn App() -> impl IntoView {
+    let dbsetup = create_local_resource(|| (), |_| async { 
+        let promiser = SqlitePromiser::use_promiser();
+
+        promiser.configure().await?;
+        let db_version = migrations::run_migrations(&promiser).await?;
+        let opfs_tree = promiser.opfs_tree().await?;
+        Ok::<_, MigrationError>((opfs_tree, db_version))
+    });
+
     let (online, set_online) = create_signal(false);
     view! {
-        <OnlineCheck set_online/>
-        <div>
-            <small>{ format!("Version: {}{}", 
-                env!("CARGO_PKG_VERSION"),
-                option_env!("BUILD_TIME")
-                    .map(|v| format!(" - {v}"))
-                    .unwrap_or("".to_string())) 
-            }</small>
-        </div>
-        <div>
-        <small>"Online: "{ move || online.get().to_string() }</small>
-        </div>
-        <Router>
-            <AppNav/>
-            <AppRoutes online/>
-        </Router>
+        <Transition fallback=move || view! {  <p>"Loading..."</p>} >
+            <ErrorBoundary fallback=|errors| view! {
+                <div style="color:red">
+                    <p>Error:</p>
+                    <ul>
+                    { move || errors.with(|v|
+                        v.iter()
+                        .map(|(_, e)| view! { <li> { format!("{:?}", e) } </li>})
+                        .collect_view())
+                    }
+                    </ul>
+                </div>
+            }>
+                <OnlineCheck set_online/>
+                <p><small>{ 
+                    format!("Version: {}{}", 
+                        env!("CARGO_PKG_VERSION"),
+                        option_env!("BUILD_TIME")
+                            .map(|v| format!(" - {v}"))
+                            .unwrap_or("".to_string())) 
+                }</small></p>
+                <p><small>"Online: "{ move || online.get().to_string() }</small></p>
+                <p><small>"DB Version: "{ dbsetup.and_then(|v| v.1) }</small></p>
+                <p><small>"Opfs tree: "{ dbsetup.and_then(|v| format!("{:#?}", v.0)) }</small></p>
+                <Router>
+                    <AppNav/>
+                    <AppRoutes online/>
+                </Router>
+            </ErrorBoundary>
+        </Transition>
     }
 }
