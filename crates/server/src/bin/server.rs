091e5e9d334decb0d226c899e98e9c7381cab73e
daniel_ <cs2dsb@gmail.com>
2024-06-27 22:58:37 +0100

    First pass at a PWA web manifest

--- crates/server/src/bin/server.rs
+++ crates/server/src/bin/server.rs
@@ -219,10 +219,9 @@ async fn main() -> Result<(), anyhow::Error> {
                     .filter_map(|(user, r)| r.err().map(|e| (user, e)))
                     .partition(|(_, err)| match err {
                         // TODO: there might be some other errors this behaviour should apply to
-                        WebPushError::EndpointNotValid 
+                        WebPushError::EndpointNotValid
                         | WebPushError::InvalidUri
-                        | WebPushError::EndpointNotFound 
-                            => false,
+                        | WebPushError::EndpointNotFound => false,
                         _ => true,
                     });
 
